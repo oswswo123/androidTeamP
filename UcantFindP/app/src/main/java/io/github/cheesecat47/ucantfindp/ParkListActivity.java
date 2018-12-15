9a0a4136c88e44a56da3840c20f24905c1c33c79
Steven Shin <cheesecat47@gmail.com>
2018-12-15 15:11:33 +0900

    예약하기 버튼 확인
    일단 토스트로 작동 확인.

--- UcantFindP/app/src/main/java/io/github/cheesecat47/ucantfindp/ParkListActivity.java
+++ UcantFindP/app/src/main/java/io/github/cheesecat47/ucantfindp/ParkListActivity.java
@@ -5,8 +5,11 @@ import android.app.ListActivity;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -29,14 +32,23 @@ public class ParkListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_park_list);
+        Log.i("TAG", "ParkListActivity: start");
 
         //테스트용. 나중에 서버 연결하면 서버에서 데이터 받아와서 출력하도록 수정해야됩니다.
         total_ParkInfo.add(new ParkInfo("Sin 주차장", 25, 15));
         total_ParkInfo.add(new ParkInfo("Cosin 주차장", 20, 13));
         total_ParkInfo.add(new ParkInfo("Tan 주차장", 40, 27));
+        Log.i("TAG", "ParkListActivity: add parkinfo activity");
 
         listView = (ListView)findViewById(R.id.activity_park_list_listView);    //리스트뷰 등록
         parkListAdapter = new ParkListAdapter(this, total_ParkInfo);   //어댑터 등록. Context - this, ParkListActivity에서 쓸거고 배열 넘겨줍니다.
         listView.setAdapter(parkListAdapter);                                   //어댑터 연결
+        Log.i("TAG", "ParkListActivity: set listview adapter");
+    }
+
+    public void onClick_ReserveBtn(View view) {
+        //ReserveBtn을 눌렀을 때 할 onClickListener
+        Toast.makeText(ParkListActivity.this, "onClick_ReserveBtn", Toast.LENGTH_SHORT).show();
+        Log.i("TAG", "ParkListActivity: onClick_ReserveBtn");
     }
 }
--- UcantFindP/app/src/main/java/io/github/cheesecat47/ucantfindp/SplashActivity.java
+++ UcantFindP/app/src/main/java/io/github/cheesecat47/ucantfindp/SplashActivity.java
@@ -3,6 +3,7 @@ package io.github.cheesecat47.ucantfindp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 /*
     스플래시 화면입니다.
@@ -13,6 +14,7 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Log.i("TAG", "SplashActivity: start");
         Intent intent = new Intent(this, ParkListActivity.class);
         //                                              LogInActivity.class 만들고 그 쪽으로 이동.
         startActivity(intent);
