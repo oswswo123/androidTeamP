8500f0264cf8bcafcb96b7e7e98abce3cbe8be37
Oh Seung Hwan <oswswo123@gmail.com>
2018-12-21 00:12:49 +0900

    ParkingLot 색상, TopText 글자크기 수정

--- UcantFindP/app/src/main/java/io/github/cheesecat47/ucantfindp/ParkingLot.java
+++ UcantFindP/app/src/main/java/io/github/cheesecat47/ucantfindp/ParkingLot.java
@@ -34,12 +34,12 @@ public class ParkingLot extends AppCompatActivity {
         String str = "■ 주차가능  ■ 주차불가  ■ 내차위치";
         SpannableStringBuilder spb1 = new SpannableStringBuilder(str);
         // SpannableStringBuilder를 이용해서 부분별로 색깔이랑 사이즈를 변경
-        spb1.setSpan(new ForegroundColorSpan(Color.parseColor("#FAB406")), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spb1.setSpan(new AbsoluteSizeSpan(100), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spb1.setSpan(new ForegroundColorSpan(Color.parseColor("#afabab")), 8, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spb1.setSpan(new AbsoluteSizeSpan(100), 8, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spb1.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorOurYellow)), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spb1.setSpan(new AbsoluteSizeSpan(80), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spb1.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorOurGrey)), 8, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spb1.setSpan(new AbsoluteSizeSpan(80), 8, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         spb1.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorOurPurple)), 16, 17, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spb1.setSpan(new AbsoluteSizeSpan(100), 16, 17, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spb1.setSpan(new AbsoluteSizeSpan(80), 16, 17, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         TopText.append(spb1);
 
         // 아직 주차버튼 리스너는 없어요... 이걸 어떻게 구현해야 할까요...?
